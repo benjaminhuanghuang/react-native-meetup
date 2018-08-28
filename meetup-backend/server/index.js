da403c47453f2271b9814dea2ac3aa28af53e483
Benjamin Huang <bhuang@rms.com>
2018-08-27 18:50:55 -0700

    add middleware

--- meetup-backend/server/index.js
+++ meetup-backend/server/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 
 import dbConfig from './config/db';
+import middlewareConfig from './config/middlewares';
 
 dbConfig();
 
 const app = express();
+middlewareConfig(app);
+
 const PORT = process.env.PORT || 8964;
 
 app.listen(PORT, err => {
