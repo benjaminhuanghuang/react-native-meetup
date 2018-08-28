b406edaa7be36d256120d679ea3c8e076dfd1508
Benjamin Huang <bhuang@rms.com>
2018-08-27 18:41:10 -0700

    connect to db

---
+++ meetup-backend/server/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+
+export default () => {
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://admin:admin1234@ds133632.mlab.com:33632/meetup');
+    mongoose.connection
+        .once('open', () => { console.log('Connected to mongodb') })
+        .on('err', err => console.error(err))
+}
--- meetup-backend/server/index.js
+++ meetup-backend/server/index.js
@@ -1,5 +1,9 @@
 import express from 'express';
 
+import dbConfig from './config/db';
+
+dbConfig();
+
 const app = express();
 const PORT = process.env.PORT || 8964;
 
